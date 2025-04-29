d67d8824128cb9229734fead51920679878f0464
ArziTech <wawcam123@gmail.com>
2025-04-29 08:26:45 +0700

    fiixing get image path function

--- actions/image.ts
+++ actions/image.ts
@@ -4,7 +4,7 @@ import ImageKit from "imagekit";
 const imageKit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
   publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-  urlEndpoint: `https://ik.imagekit.io/arzitech/`,
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
 });
 
 export async function getImagePathById(fileId: string): Promise<string> {
